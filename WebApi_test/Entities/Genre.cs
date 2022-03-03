f89ba20a31d9548c91d13132a6bb7c2d5598e926
unknown <joyjk>
2022-03-03 15:42:57 +0600

    updated

--- WebApi_test/Entities/Genre.cs
+++ WebApi_test/Entities/Genre.cs
@@ -1,12 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebApi_test.Validations;
 
 namespace WebApi_test.Entities
 {
-    public class Genre
+    public class Genre: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="{0} is needed")]
         [StringLength(5)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
+        [CreditCard]
+        public string CreditCard { get; set; }
+        [Url]
+        public string Url { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+                if(firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be upper case", new string[] { nameof(Name) });
+                }
+            }
+        }
     }
 }
