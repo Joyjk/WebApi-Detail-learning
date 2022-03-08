19d63e88e39fb068b66b777e3551f86d0aec11d2
unknown <joyjk>
2022-03-08 16:35:20 +0600

    Config added

---
+++ WebApi_test/Controllers/ConfigurationsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi_test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ConfigurationsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(configuration["lastName"]);
+        }
+    }
+}
